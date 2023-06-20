7d47e5a30391b9bc03ffe285a072b00c1319f3dd
Alejandro <aobregonrodri@gmail.com>
2023-06-20 23:00:55 +0200

    g

--- routers/obtainNiveles.js
+++ routers/obtainNiveles.js
@@ -5,16 +5,17 @@ const express = require('express');
 const routeNivels = express.Router();
 
 
-routeNivels.get('/', async (req, res) => {
+routeNivels.post('/', async (req, res) => {
     const conectionSQL = req.conexionIndustry;
     console.log('preparando la conexion')
-    console.log(req.body)
-    console.log(req.body)
+    // console.log(req.body)
+    // console.log(req.body)
     // console.log(conectionSQL)
 
     try {
-        // const result = await conectionSQL.query("SELECT * FROM ObtenerNivel('482','4200','4210')")
+        const result = await conectionSQL.query("SELECT * FROM ObtenerNivel('482','4200','4210')")
         // console.log(result)
+        res.status(200).send(result.recordset)
     } catch (error) {
         console.log(error)
     }
